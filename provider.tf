5b97e7b34918db90896e6e52488d9b0969e19258
Tiger4Code <light.email.work@gmail.com>
2024-08-20 20:50:15 -0400

    wip

--- provider.tf
+++ provider.tf
@@ -4,8 +4,15 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    storage_account_name = "noorstorageaccount2024"
+    storage_account_name = "noorterraformstatest"
     container_name       = "terraform-state"
     key                  = "noor-azure-web-iac.terraform.tfstate"
+    access_key           = var.storage_account_access_key
   }
+}
+
+# STORAGE_ACCOUNT_ACCESS_KEY
+variable "storage_account_access_key" {
+  description = "The public SSH key to use for the virtual machine."
+  type        = string
 }
