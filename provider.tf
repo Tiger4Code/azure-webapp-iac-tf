eb3ce451850502bd09c106f6caea91efe26c99ec
Tiger4Code <light.email.work@gmail.com>
2024-08-21 07:29:33 -0400

    add storage account access key to plan workflow

--- provider.tf
+++ provider.tf
@@ -7,12 +7,12 @@ terraform {
     storage_account_name = "noorterraformstatest"
     container_name       = "terraform-state"
     key                  = "noor-azure-web-iac.terraform.tfstate"
-    access_key           = var.storage_account_access_key
+    # access_key           = var.storage_account_access_key
   }
 }
 
 # STORAGE_ACCOUNT_ACCESS_KEY
-variable "storage_account_access_key" {
-  description = "The public SSH key to use for the virtual machine."
-  type        = string
-}
+# variable "storage_account_access_key" {
+#   description = "The public SSH key to use for the virtual machine."
+#   type        = string
+# }
