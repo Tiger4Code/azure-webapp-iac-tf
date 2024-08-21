9579beecbb2a7b0a16caa1d865fe076b78439162
Tiger4Code <light.email.work@gmail.com>
2024-08-20 20:25:11 -0400

    remote state

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "azurerm" {
   features {}
 }
+
+terraform {
+  backend "azurerm" {
+    storage_account_name = "noorstorageaccount2024"
+    container_name       = "terraform-state"
+    key                  = "noor-azure-web-iac.terraform.tfstate"
+  }
+}
